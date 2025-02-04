687424ebd97975bea44a035259cd0b7b26d6afb3
samarth-thaker <samarth.thaker07@gmail.com>
2025-02-04 16:30:43 +0530

    Group/group tile integration---got error

--- lib/screens/group.dart
+++ lib/screens/group.dart
@@ -1,96 +1,117 @@
 import 'package:flutter/material.dart';
 import 'package:splitwise/widgets/groupTile.dart';
-import 'package:splitwise/widgets/customButton.dart';
 
 class Groups extends StatefulWidget {
   const Groups({super.key});
+  
   @override
   State<Groups> createState() => _GroupsState();
 }
 
-final TextEditingController groupController = TextEditingController();
-final TextEditingController amountController = TextEditingController();
+class _GroupsState extends State<Groups> {
+  final TextEditingController groupController = TextEditingController();
+  final TextEditingController amountController = TextEditingController();
 
-void addGroup(BuildContext context) {
-  showModalBottomSheet(
-    context: context,
-    isScrollControlled: true, 
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
-    ),
-    builder: (BuildContext context) {
-      return Padding(
-        padding: EdgeInsets.only(
-          left: 16,
-          right: 16,
-          bottom: MediaQuery.of(context).viewInsets.bottom + 16, 
-          top: 16,
-        ),
-        child: Column(
-          mainAxisSize: MainAxisSize.min, 
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: Container(
-                height: 5,
-                width: 40,
-                decoration: BoxDecoration(
-                  color: Colors.grey[400],
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-            ),
-            SizedBox(height: 16),
-            Text(
-              "Create a Group",
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 16),
-            TextField(
-              controller: groupController,
-              decoration: InputDecoration(
-                labelText: 'Group Name',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height: 12),
-            TextField(
-              controller: amountController,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: 'Enter Amount',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height: 20),
-            SizedBox(
-              width: double.infinity, 
-              child: ElevatedButton(
-                onPressed: () {
-                    
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blue,
-                  padding: EdgeInsets.symmetric(vertical: 14),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
+  List<Map<String, dynamic>> groupTile = [
+    {"groupname": 'Cup of tea', "groupBalance": 500},
+    {"groupname": 'Manali', "groupBalance": 1500},
+  ];
+
+  void addGroup(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
+      ),
+      builder: (BuildContext context) {
+        return Padding(
+          padding: EdgeInsets.only(
+            left: 16,
+            right: 16,
+            bottom: MediaQuery.of(context).viewInsets.bottom + 16,
+            top: 16,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Container(
+                  height: 5,
+                  width: 40,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[400],
+                    borderRadius: BorderRadius.circular(10),
                   ),
                 ),
-                child: Text(
-                  "Add Group",
-                  style: TextStyle(fontSize: 16, color: Colors.white),
+              ),
+              SizedBox(height: 16),
+              Text(
+                "Create a Group",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 16),
+              TextField(
+                controller: groupController,
+                decoration: InputDecoration(
+                  labelText: 'Group Name',
+                  border: OutlineInputBorder(),
                 ),
               ),
-            ),
-          ],
-        ),
-      );
-    },
-  );
-}
+              SizedBox(height: 12),
+              TextField(
+                controller: amountController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Enter Amount',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 20),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    String groupName = groupController.text;
+                    double? amount = double.tryParse(amountController.text);
 
+                    if (groupName.isNotEmpty && amount != null) {
+                      setState(() {
+                        
+                        groupTile.add({"groupname": groupName, "groupBalance": amount});
+                      });
+                      groupController.clear();
+                      amountController.clear();
+                      Navigator.pop(context);
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    padding: EdgeInsets.symmetric(vertical: 14),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  child: Text(
+                    "Add Group",
+                    style: TextStyle(fontSize: 16, color: Colors.white),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
 
-class _GroupsState extends State<Groups> {
+  void deleteGroup(dynamic index) {
+    setState(() {
+      groupTile.removeAt(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -109,12 +130,19 @@ class _GroupsState extends State<Groups> {
           padding: EdgeInsets.all(16),
           child: Column(
             children: [
-              SearchBar(),
-              SizedBox(
-                height: 15,
-              ),
-              Grouptile(groupname: 'Cup of tea', groupBalance: 500),
-              Grouptile(groupname: 'Manali', groupBalance: 1500),
+             
+               SearchBar(),
+              SizedBox(height: 15),
+             
+              ...groupTile.map((group) {
+                return Grouptile(
+                  groupname: group['groupname'],
+                  groupBalance: group['groupBalance'],
+                  onDelete: () {
+                    deleteGroup(groupTile.indexOf(group));
+                  },
+                );
+              }).toList(),
             ],
           ),
         ),
--- lib/widgets/groupTile.dart
+++ lib/widgets/groupTile.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
 
 class Grouptile extends StatefulWidget {
-  final String groupname;
+  String groupname;
   final double groupBalance;
-  
-  const Grouptile(
-      {required this.groupname, required this.groupBalance, Key? key})
+
+  final VoidCallback? onDelete;
+  Grouptile(
+      {required this.groupname,
+      required this.groupBalance,
+      this.onDelete,
+      Key? key})
       : super(key: key);
 
   @override
   State<Grouptile> createState() => _GrouptileState();
 }
+
+final TextEditingController newGroupNameController = TextEditingController();
+
 
 class _GrouptileState extends State<Grouptile> {
   @override
@@ -23,7 +30,7 @@ class _GrouptileState extends State<Grouptile> {
         color: Colors.white,
         borderRadius: BorderRadius.circular(8),
         boxShadow: [
-          BoxShadow(    
+          BoxShadow(
             color: Colors.grey.withOpacity(0.2),
             blurRadius: 4,
             spreadRadius: 1,
@@ -50,109 +57,153 @@ class _GrouptileState extends State<Grouptile> {
           const SizedBox(width: 5),
           IconButton(
             icon: Icon(Icons.edit_sharp),
-            onPressed: ()=>{
-              showModalBottomSheet(context: context, builder: (BuildContext context)
-              {
-                return Padding(padding: EdgeInsets.only(
-              bottom: MediaQuery.of(context).viewInsets.bottom,
-              top: 15,
-              right: 15,
-              left: 15,
-            ),
-            child: Column(
-              children: [
-                TextField(decoration: InputDecoration(hintText: 'Edit group name'),),
-                
-                TextButton(child: Text('Save', style: TextStyle(
-                  fontSize: 15,
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                ),), onPressed: ()=>{})
-              ],
-            ),
-            );
-              })
+            onPressed: () => {
+              showModalBottomSheet(
+                context: context,
+                isScrollControlled: true,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
+                ),
+                builder: (BuildContext context) {
+                  return Padding(
+                    padding: EdgeInsets.only(
+                      bottom: MediaQuery.of(context).viewInsets.bottom + 16,
+                      top: 16,
+                      left: 16,
+                      right: 16,
+                    ),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          height: 5,
+                          width: 40,
+                          decoration: BoxDecoration(
+                            color: Colors.grey[400],
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        SizedBox(height: 16),
+                        TextField(
+                          controller: newGroupNameController,
+                          decoration: InputDecoration(
+                              hintText: 'Edit group name',
+                              border: OutlineInputBorder()),
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.w500),
+                        ),
+                        SizedBox(height: 20),
+                        SizedBox(
+                          width: double.infinity,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                widget.groupname = newGroupNameController.text;
+                              });
+                              Navigator.pop(context);
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.blueAccent,
+                              padding: EdgeInsets.symmetric(vertical: 14),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                            ),
+                            child: Text(
+                              "Confirm",
+                              style:
+                                  TextStyle(fontSize: 16, color: Colors.white),
+                            ),
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () => Navigator.pop(context),
+                          child: Text(
+                            "Cancel",
+                            style: TextStyle(fontSize: 16, color: Colors.black),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              )
             },
           ),
-          const SizedBox(width: 5,),
-           IconButton(
+          const SizedBox(
+            width: 5,
+          ),
+          IconButton(
             icon: Icon(Icons.delete_sharp),
-            onPressed: ()=>{
-              showModalBottomSheet(
-  context: context,
-  isScrollControlled: true, // Ensures proper expansion
-  shape: RoundedRectangleBorder(
-    borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
-  ),
-  builder: (BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(
-        bottom: MediaQuery.of(context).viewInsets.bottom + 16, // Adjust for keyboard
-        top: 16,
-        left: 16,
-        right: 16,
-      ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-         
-          Container(
-            height: 5,
-            width: 40,
-            decoration: BoxDecoration(
-              color: Colors.grey[400],
-              borderRadius: BorderRadius.circular(10),
+            onPressed: () {
+  showModalBottomSheet(
+    context: context,
+    isScrollControlled: true,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
+    ),
+    builder: (BuildContext context) {
+      return Padding(
+        padding: EdgeInsets.only(
+          bottom: MediaQuery.of(context).viewInsets.bottom + 16,
+          top: 16,
+          left: 16,
+          right: 16,
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              height: 5,
+              width: 40,
+              decoration: BoxDecoration(
+                color: Colors.grey[400],
+                borderRadius: BorderRadius.circular(10),
+              ),
             ),
-          ),
-          SizedBox(height: 16),
-
-          
-          Text(
-            "Are you sure you want to delete this group?",
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
-          ),
-          SizedBox(height: 20),
-
-          
-          SizedBox(
-            width: double.infinity,
-            child: ElevatedButton(
-              onPressed: () {
-                
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.redAccent,
-                padding: EdgeInsets.symmetric(vertical: 14),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8),
+            SizedBox(height: 16),
+            Text(
+              "Are you sure you want to delete this group?",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+            ),
+            SizedBox(height: 20),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  widget.onDelete?.call(); // ✅ Call the delete function
+                  Navigator.pop(context);
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.redAccent,
+                  padding: EdgeInsets.symmetric(vertical: 14),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+                child: Text(
+                  "Delete",
+                  style: TextStyle(fontSize: 16, color: Colors.white),
                 ),
               ),
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context),
               child: Text(
-                "Delete",
-                style: TextStyle(fontSize: 16, color: Colors.white),
+                "Cancel",
+                style: TextStyle(fontSize: 16, color: Colors.black),
               ),
             ),
-          ),
+          ],
+        ),
+      );
+    },
+  );
+},
 
-          
-          TextButton(
-            onPressed: () => Navigator.pop(context), 
-            child: Text(
-              "Cancel",
-              style: TextStyle(fontSize: 16, color: Colors.black),
-            ),
-          ),
-        ],
-      ),
-    );
-            }
-              
-  
-)
-
-            },
           ),
         ],
       ),
